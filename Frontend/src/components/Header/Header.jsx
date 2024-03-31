677dd2b474a90368064724da4d0c51008b4f3132
mai <maidangthanh99@gmail.com>
2024-04-01 01:46:46 +0700

    update home page

--- Frontend/src/components/Header/Header.jsx
+++ Frontend/src/components/Header/Header.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
-import {Container, Row, Button} from 'reactstrap'
-import {NavLink, Link} from 'react-router-dom'
+import { Container, Row, Button } from 'reactstrap'
+import { NavLink, Link } from 'react-router-dom'
 import logo from '../../assets/images/logo.png'
-
+import "./header.css"
 const nav__links = [
    {
       path: '/home',
@@ -19,8 +19,8 @@ const nav__links = [
 ]
 
 const Header = () => {
-  return (
-    <header className='header' >
+   return (
+      <header className='header' >
          <Container>
             <Row>
                <div className="nav__wrapper d-flex align-items-center justify-content-between">
@@ -29,14 +29,14 @@ const Header = () => {
                      <img src={logo} alt="" />
                   </div>
                   {/* ========== LOGO END ========== */}
-                  
+
                   {/* ========== MENU START ========== */}
                   <div className="navigation" >
                      <ul className="menu d-flex align-items-center gap-5">
                         {
                            nav__links.map((item, index) => (
                               <li className="nav__item" key={index}>
-                                 <NavLink to={item.path}>{item.display}</NavLink>
+                                 <NavLink to={item.path} className={navClass => navClass.isActive ? 'active__link' : ''}>{item.display}</NavLink>
                               </li>
                            ))
                         }
@@ -50,12 +50,12 @@ const Header = () => {
                      </div>
                      <span className="mobile__menu" ></span>
                      <i class="ri-menu-line"></i>
-                </div>
-                </div>
-              </Row>
-            </Container>
-          </header>
-  )
+                  </div>
+               </div>
+            </Row>
+         </Container>
+      </header>
+   )
 }
 
 export default Header
